dc935d93e79f027dc50f9cc8b3dee0a2a5ea1bb5
kalinkochnev <kalin.kochnev@gmail.com>
2023-09-02 21:05:20 -0400

    Setup C++ dev environment with arduino-mk, clangd, bear

---
+++ arduino/main.cpp
@@ -0,0 +1,18 @@
+#ifdef ARDUINO
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#endif // ARDUINO
+
+int main()
+{
+	init();
+	while (true) {
+        digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+        delay(1000);                      // wait for a second
+        digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+        delay(1000); 
+	}
+}
